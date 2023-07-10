931ec007dc0c8b9615bf9ee288398232e3d160f9
Maurice Koreman <mg.koreman99@gmail.com>
2023-07-10 20:34:10 +0200

    smol change

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -2,9 +2,9 @@ import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().default(5432).required(),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  DB_DATABASE: Joi.string().required(),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().default(5432).required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
 });
