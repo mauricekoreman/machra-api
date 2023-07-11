5cfe58deb8b987eda885a83af37078c457c86f96
Maurice Koreman <mg.koreman99@gmail.com>
2023-07-11 22:47:00 +0200

    remove login validator checks

--- src/auth/dto/user-credentials.dto.ts
+++ src/auth/dto/user-credentials.dto.ts
@@ -1,17 +1,9 @@
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class UserCredentialsDto {
   @IsString()
-  @MinLength(4)
-  @MaxLength(20)
   username: string;
 
   @IsString()
-  @MinLength(8)
-  @MaxLength(32)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message:
-      'Password must contain at least: 1 uppercase letter, 1 lowercase letter and 1 number or special character',
-  })
   password: string;
 }
